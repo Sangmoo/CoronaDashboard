5c6102f04921ad5447b5df5f891293125effaf5f
Sangmoo <first_1st@naver.com>
2020-08-23 00:29:00 +0900

    Deploy

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # CoronaDashboard
 
-Using Package
+Using Pip Package
 
-* pandas
-* virtualenvwrapper-win
-* dash
-* jupyterlab
-* jupyterthemes - monokai
+- dash==1.14.0
+- dash-core-components==1.10.2
+- dash-html-components==1.0.3
+- pandas==1.1.0
+- plotly==4.9.0
+- virtualenvwrapper-win
+- jupyterlab
+- jupyterthemes - monokai
 
 Data Ref - https://github.com/CSSEGISandData/COVID-19
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
-from data import countries_df, totals_df
+from data import (
+    countries_df,
+    totals_df,
+    dropdown_options,
+    make_global_df,
+    make_country_df,
+)
 from builders import make_table
 
 
@@ -14,6 +21,8 @@ stylesheets = [
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
+server = app.server
+
 bubble_map = px.scatter_geo(
     countries_df,
     size="Confirmed",
@@ -21,6 +30,7 @@ bubble_map = px.scatter_geo(
     color="Confirmed",
     color_continuous_scale=px.colors.sequential.Oryel,
     locations="Country_Region",
+    projection="equirectangular",
     locationmode="country names",
     title="Confirmed By Country",
     size_max=50,
@@ -35,7 +45,9 @@ bubble_map = px.scatter_geo(
     },
 )
 
-bubble_map.update_layout(margin=dict(l=0, r=0, t=50, b=0))
+bubble_map.update_layout(
+    margin=dict(l=0, r=0, t=50, b=0), coloraxis_colorbar=dict(xanchor="left", x=0)
+)
 
 bars_graph = px.bar(
     totals_df,
@@ -80,10 +92,49 @@ app.layout = html.Div(
                 "gap": 50,
                 "gridTemplateColumns": "repeat(4, 1fr)",
             },
-            children=[html.Div(children=[dcc.Graph(figure=bars_graph)]),],
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_graph)]),
+                html.Div(
+                    style={"grid-column": "span 3"},
+                    children=[
+                        dcc.Dropdown(
+                            style={
+                                "width": 320,
+                                "margin": "0 auto",
+                                "color": "#111111",
+                            },
+                            placeholder="Select a Country",
+                            id="country",
+                            options=[
+                                {"label": country, "value": country}
+                                for country in dropdown_options
+                            ],
+                        ),
+                        dcc.Graph(id="country_graph"),
+                    ],
+                ),
+            ],
         ),
     ],
 )
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+@app.callback(Output("country_graph", "figure"), [Input("country", "value")])
+def update_hello(value):
+    if value:
+        df = make_country_df(value)
+    else:
+        df = make_global_df()
+    fig = px.line(
+        df,
+        x="date",
+        y=["confirmed", "deaths", "recovered"],
+        template="plotly_dark",
+        labels={"value": "Cases", "variable": "Condition", "date": "Date"},
+        hover_data={"value": ":,", "variable": False, "date": False},
+    )
+    fig.update_xaxes(rangeslider_visible=True)
+    fig["data"][0]["line"]["color"] = "#e74c3c"
+    fig["data"][1]["line"]["color"] = "#8e44ad"
+    fig["data"][2]["line"]["color"] = "#27ae60"
+    return fig
